a1074154f2942e28a81073775a50d0d741fb63fc
Nick Stenning <nick@whiteink.com>
2011-06-22 21:08:32 +0100

    Specify deps directly in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'epsrc-scraper',
-
     version = '0.1',
-
     packages = find_packages(),
-
-    install_requires = [],
-
+    install_requires = [
+        'mechanize==0.2.5',
+        'pyquery==0.7'
+    ],
     entry_points = {
         'console_scripts': [
             'epsrc-scrape = epsrc.command.scrape:main',
